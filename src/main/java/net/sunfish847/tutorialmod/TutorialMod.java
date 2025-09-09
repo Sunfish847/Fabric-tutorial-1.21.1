3a4f2813022b9b511fcdecf54743cea92e62152b
Sunfish847 <taxthedeparted@gmail.com>
2025-09-09 10:07:02 -0600

    added blocks and item groups

--- src/main/java/net/sunfish847/tutorialmod/TutorialMod.java
+++ src/main/java/net/sunfish847/tutorialmod/TutorialMod.java
@@ -2,6 +2,8 @@ package net.sunfish847.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.sunfish847.tutorialmod.block.ModBlocks;
+import net.sunfish847.tutorialmod.item.ModItemGroups;
 import net.sunfish847.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +19,8 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+		ModItemGroups.registerItemGroups();
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
